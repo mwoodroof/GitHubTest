3130bcbea74ab75a525e89a4e9b73bc6f16aa95e
Woodroof <MWOODROO@ford.com>
2019-06-28 15:07:36 +0100

    Added task 4 & 5

---
+++ Task4.c
@@ -0,0 +1,18 @@
+//Tutorial for Monday 24th June
+//Task 4
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	int i;
+	int arr[5] = {0, 0, 1, 2, 3};
+
+//	{
+		for (i=0; i < 5; i++)
+			printf("%d \n", arr[i]);
+//	}
+
+	return(0);
+}
---
+++ Task5.c
@@ -0,0 +1,19 @@
+//Tutorial for Monday 24th June
+//Task 5
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	int num=0;
+	printf("Please enter a number between 5 and 15: \n\n");
+	scanf("%d", num);
+	printf("you typed %d",num);
+//	if a<10
+//		printf("%d is less than 10", a);
+//	else
+//		printf("%d is 10 or more", a);
+
+	return(0);
+}
